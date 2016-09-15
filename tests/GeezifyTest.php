d09e3b2573caa1c6122cce55fc764b03803b3636
Sam As End <4sam21@gmail.com>
2016-09-16 00:28:16 +0300

    back to rand
    https://travis-ci.org/andegna/geezify/builds/160293338

--- tests/GeezifyTest.php
+++ tests/GeezifyTest.php
@@ -12,7 +12,7 @@ class GeezifyTest extends TestCase
         $geezify = Geezify::create();
 
         for ($i = 0; $i < 10000; $i++) {
-            $random_number = random_int(1, 9999999999);
+            $random_number = rand(1, 9999999999);
 
             $geez_number = $geezify->toGeez($random_number);
             $ascii_number = $geezify->toAscii($geez_number);
