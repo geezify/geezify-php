756cdffe7ba872e6d64298c05ddfc4e773fd61b7
Sam As End <4sam21@gmail.com>
2017-02-20 00:17:32 +0300

    Lets bump up the PHP version with PHPUnit

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,7 @@
 
 namespace Andegna\Geez\PHPUnit;
 
-class TestCase extends \PHPUnit_Framework_TestCase
+class TestCase extends \PHPUnit\Framework\TestCase
 {
     public function geezNumberTestDataProvider()
     {
